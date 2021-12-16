44e4e2cd53590f427e6f049049f5b1f10cce0202
Tony Allen <tony@allen.gg>
2021-12-16 12:14:24 -0800

    lol super broken aggregator grpc server wip
    Signed-off-by: Tony Allen <tony@allen.gg>

--- src/aggregator/server/grpc/server.go
+++ src/aggregator/server/grpc/server.go
@@ -1,18 +1,94 @@
 package grpc
 
 import (
+	"fmt"
+	"net"
+	"net/http"
+
 	"github.com/m3db/m3/src/aggregator/aggregator"
 	"github.com/m3db/m3/src/aggregator/generated/flatbuffer"
 	xserver "github.com/m3db/m3/src/x/server"
+	"google.golang.org/grpc"
 )
 
 type grpcAggregatorServer struct {
 	flatbuffer.AggregatorServer
 }
 
+type server struct {
+	srv grpcAggregatorServer
+}
+
+    // ListenAndServe forever listens to new incoming connections and
+    // handles data from those connections.
+    ListenAndServe() error
+
+    // Serve accepts and handles incoming connections on the listener l forever.
+    Serve(l net.Listener) error
+
+    // Close closes the server.
+    Close()
+
 // TODO: add options
 func NewServer(address string, aggregator aggregator.Aggregator) (xserver.Server, error) {
 	return nil, nil
+}
+
+func (s *grpcAggregatorServer) ListenAndServe() error {
+	// TODO @tallen configure where to listen.
+	listener, err := net.Listen("tcp", "localhost:13370")
+	if err != nil {
+		return err
+	}
+
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+
+	pb.RegisterRouteGuideServer(grpcServer, newServer())
+	grpcServer.Serve(lis)
+
+	return s.Serve(listener)
+}
+
+func (s *grpcAggregatorServer) Serve(l net.Listener) error {
+	/*
+	registerHandlers(s.opts.Mux(), s.aggregator)
+
+	// create and register debug handler
+	debugWriter, err := xdebug.NewZipWriterWithDefaultSources(
+		defaultCPUProfileDuration,
+		s.iOpts,
+	)
+	if err != nil {
+		return fmt.Errorf("unable to create debug writer: %v", err)
+	}
+
+	if err := debugWriter.RegisterHandler(xdebug.DebugURL, s.opts.Mux()); err != nil {
+		return fmt.Errorf("unable to register debug writer endpoint: %v", err)
+	}
+
+	server := http.Server{
+		Handler:      s.opts.Mux(),
+		ReadTimeout:  s.opts.ReadTimeout(),
+		WriteTimeout: s.opts.WriteTimeout(),
+	}
+
+	s.listener = l
+	s.address = l.Addr().String()
+
+	go func() {
+		server.Serve(l)
+	}()
+	*/
+
+	return nil
+}
+
+func (s *grpcAggregatorServer) Close() {
+	if s.listener != nil {
+		s.listener.Close()
+	}
+	s.listener = nil
 }
 
 func (srv *grpcAggregatorServer) WriteUntimedCounter(flatbuffer.Aggregator_WriteUntimedCounterServer) error {
